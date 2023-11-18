c746f5302d3ca561126dc01b7d82b2d9c9fb85e8
Tochiepet <alepeter215@gmail.com>
2023-11-17 22:13:10 -0800

    small changes

--- emomorye.c
+++ emomorye.c
@@ -1,18 +1,13 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
+ * kohytr
+ * lopytre
  */
-int bfree(void **ptr)
 {
-	if (ptr && *ptr)
-	{
-		free(*ptr);
-		*ptr = NULL;
-		return (1);
+
+	*ptr = NULL;
+	return (1);
 	}
-	return (0);
+return (0);
 }
